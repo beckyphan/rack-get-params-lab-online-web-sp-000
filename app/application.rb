39785e06485a38deba6d8efc829f1f989c0573fb
Becky Phan <beckyphan@live.com>
2019-08-03 18:07:09 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -22,7 +22,7 @@ class Application
       end 
     elsif req.path.match(/add/)
       item_to_add = req.params["a"]
-      resp.write add_to_cart(item)
+      resp.write add_to_cart(item_to_add)
     else
       resp.write "Path Not Found"
     end
